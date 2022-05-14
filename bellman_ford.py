40004c6e01326d9ba8cf6e2da63432b1b8ceee2d
David Sand <davidstsand@gmail.com>
2022-05-14 13:11:54 -0300

    Create bellman ford algorithm

---
+++ bellman_ford.py
@@ -0,0 +1,41 @@
+import numpy as np
+from grafo import GrafoNaoDirigido
+
+def bellman_ford(grafo: GrafoNaoDirigido, vertice_inicial: int):
+    D = {vertice: np.inf for vertice in grafo.vertices}
+    A = {vertice: None for vertice in grafo.vertices}
+    D[vertice_inicial] = 0
+
+    # Question: Why do we iterate over the vertices?
+    for _ in range(1, grafo.qtdVertices()):
+        for u, v in grafo.arestas:
+            if D[v] > D[u] + grafo.pesos[str((u, v))]:
+                D[v] = D[u] + grafo.pesos[str((u, v))]
+                A[v] = u
+
+    for u, v in grafo.arestas:
+        if D[v] > D[u] + grafo.pesos[str((u, v))]:
+            return False, None, None
+
+    return True, D, A
+
+
+if __name__ == '__main__':
+    arquivo = 'facebook_santiago.net'
+    grafo = GrafoNaoDirigido(arquivo)
+    vertice_inicial = 2
+
+    _, D, A = bellman_ford(grafo, vertice_inicial)
+
+    for vertice, distancia in list(D.items()):
+        if distancia == np.inf:
+            continue
+
+        vertice_anterior = A[vertice]
+        caminho = []
+        while vertice_anterior is not None:
+            caminho.append(str(vertice_anterior))
+            vertice_anterior = A[vertice_anterior]
+
+        caminho = [str(vertice)] + caminho
+        print(f"{vertice}: {', '.join(reversed(caminho))}; d={distancia}")
